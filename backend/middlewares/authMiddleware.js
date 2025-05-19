bdc4712971951059f818ea293242210f119097d7
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2025-05-20 01:58:31 +0700

    feat: add submit tax payment

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ const authenticateUser = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
-    next(); // ✅ Proceed to the next middleware
+    next();
   } catch (error) {
     res.status(403).json({ error: "Invalid or expired token" });
   }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const userRoutes = require("../src/routes/userRoutes");
 const authRoutes = require("../src/routes/authRoutes");
+const taxRoutes = require("../src/routes/taxRoutes");
 
 app.use("/user", userRoutes);
 app.use("/account", authRoutes);
+app.use("/tax", taxRoutes);
 
 // Run the server
 const PORT = process.env.PORT || 3000;
